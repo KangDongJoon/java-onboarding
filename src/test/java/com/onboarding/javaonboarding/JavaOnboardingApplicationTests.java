4e3ba310149f57bd652e63925a19ab507dd73115
KangDongJoon <ehdwns1119@naver.com>
2025-01-17 12:29:58 +0900

    first commit

---
+++ src/test/java/com/onboarding/javaonboarding/JavaOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onboarding.javaonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOnboardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
